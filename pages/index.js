26f47676c48a5d305698b82262f6ca65cf7621ea
Fabio Andreola <fabio.andreola@casumo.com>
2021-11-19 10:39:53 -0300

    mobile menu

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [active, setActive] = useState(false);
+  const activeSide =
+    'bg-gray-800 h-screen w-60 transform transition-all fixed duration-700 text-white flex justify-center p-2';
+  const hiddenSide =
+    'bg-gray-800 h-screen w-60 transform transition-all fixed duration-700 text-white flex justify-center p-2 -translate-x-60';
+  const activeButton =
+    'absolute w-10 h-10 bg-yellow-400 top-1 left-1 cursor-pointer transition-all transform duration-700 flex items-center justify-center';
+  const normalButton =
+    'absolute w-10 h-10 bg-yellow-400 top-1 cursor-pointer transition-all transform duration-700 flex items-center justify-center translate-x-60';
   return (
     <div className="bg-gray-50 max-w-lg drop-shadow-2xl border-l-2 border-r-2 border-gray-500">
       <Head>
         <title>Shop with Lari</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
@@ -15,12 +25,41 @@ export default function Home() {
         />
       </Head>
 
+      <div className="w-screen flex transform relative transition-all duration-1000 z-10">
+        <div className={active ? activeSide : hiddenSide}>
+          <div className="flex flex-col">
+            <a className="p-4 font-bold to" href="#services">
+              Serviços
+            </a>
+            <a className="p-4 font-bold top" href="#why-europe">
+              Porque comprar na Europa
+            </a>
+            <a className="p-4 font-bold" href="#does-it-worth">
+              Vale mesmo a pena?
+            </a>
+            <a className="p-4 font-bold" href="#redirecting">
+              Redirecionamento de Compras
+            </a>
+            <a className="p-4 font-bold" href="#contact">
+              Fale comigo
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className={active ? normalButton : activeButton}
+        onClick={() => setActive(!active)}
+      >
+        {' '}
+        &#9776;
+      </div>
+
       <header className="justify-center items-center flex min-w-full p-1 h-60 text-center header">
         <div>
-          <h1 className="text-9xl text-center font-extrabold text-yellow-400 text-opacity-90 ">
+          <h1 className="fade-in-1s text-9xl text-center font-extrabold text-yellow-400 text-opacity-90 ">
             Lari
           </h1>
-          <h2 className="text-2xl font-bold text-white fade-in">
+          <h2 className="fade-in-4s text-2xl font-bold text-white fade-in">
             personal shopper
           </h2>
         </div>
@@ -28,11 +67,11 @@ export default function Home() {
 
       <main className="flex flex-col items-center">
         <div className="min-w-full">
-          <div className="h-full flex justify-center pb-8 w-full px-8">
+          <div className="h-full flex justify-center pb-5 w-full px-8">
             <img
               src="/img/lari-perfil.jpg"
               alt="perfil lari"
-              className="w-3/4 fade-in drop-shadow-2xl shadow-2xl rounded-full mt-14 border-2 border-yellow-400 border-opacity-40 border-dotted"
+              className="w-3/4 fade-in-4s drop-shadow-2xl shadow-2xl rounded-full mt-14 border-2 border-yellow-400 border-opacity-40 border-dotted"
             />
           </div>
 
@@ -68,7 +107,10 @@ export default function Home() {
         </div>
 
         <div className="min-w-full">
-          <h2 className="text-5xl font-bold text-purple-700 m-8 text-center">
+          <h2
+            id="services"
+            className="text-5xl font-bold text-purple-700 m-8 text-center"
+          >
             Serviços
           </h2>
           <ul className="text-purple-700 text-xl font-bold px-4">
@@ -87,7 +129,10 @@ export default function Home() {
           </ul>
         </div>
         <div className="bg-purple-700 justify-center">
-          <h2 className="text-5xl font-bold m-8 p-3 text-center text-white pb-10">
+          <h2
+            id="why-europe"
+            className="text-5xl font-bold m-8 text-center text-white pb-5"
+          >
             Porque comprar na Europa <span className="text-pink-400">?</span>
           </h2>
           <div className="flex px-10">
@@ -96,7 +141,7 @@ export default function Home() {
               alt="checkmark"
               className="pr-4"
             />
-            <p className="text-center py-6 text-xl font-bold text-pink-400 bg-checkmark ml-auto mr-auto">
+            <p className="text-center pb-6 text-xl font-bold text-pink-400 bg-checkmark ml-auto mr-auto">
               Ter acesso a uma grande variedade de produtos que não são
               disponibilizados para venda no Brasil!
             </p>
@@ -219,7 +264,10 @@ export default function Home() {
           </div>
         </div>
         <div className="min-w-full text-center">
-          <h2 className="text-5xl font-bold text-purple-700 m-8">
+          <h2
+            id="does-it-worth"
+            className="text-5xl font-bold text-purple-700 m-8"
+          >
             Vale mesmo a pena<span className="text-pink-400">?</span>
           </h2>
           <p className="text-xl font-bold text-purple-700 m-8 border-t-2 border-r-2 p-4 border-dashed border-purple-700">
@@ -230,7 +278,7 @@ export default function Home() {
           </p>
         </div>
         <div className="bg-purple-700 min-w-full text-center text-white">
-          <h2 className="text-4xl font-bold my-8">
+          <h2 id="redirecting" className="text-4xl font-bold my-8">
             Redirecionamento de Compras
           </h2>
           <h3 className="text-xl font-bold text-pink-400">O que é:</h3>
@@ -296,8 +344,16 @@ export default function Home() {
             <span className="text-pink-400 px-1">frete até sua casa</span>
           </p>
         </div>
+        <div className="scroll-top">
+          <a className="scroll-top__link" href="#">
+            ^
+          </a>
+        </div>
       </main>
-      <footer className="bg-pink-500 min-w-full text-lg flex flex-col items-start px-4">
+      <footer
+        id="contact"
+        className="bg-pink-500 min-w-full text-lg flex flex-col items-start px-4"
+      >
         <div className="text-white font-bold flex items-center h-16">
           <img
             src="/img/instagram-logo-purple.png"
